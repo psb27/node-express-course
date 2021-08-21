61f84e7cc9453306190e019b84ee3f5d3dfca420
psb27 <singhprateek2704@gmail.com>
2021-08-21 21:42:50 +0530

    add dyanamic GET route

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ const express = require('express');
 const app = express();
 
 const mockUserData = [
-    { name: 'Prateek' }
+    { name: 'Prateek' },
     {name: 'Shudhansu'}
 ]
 
 app.get('/users', function (req, res) {
     res.json({
-        success: true;
+        success: true,
         message: 'successfully got users. Nice!',
         users: mockUserData
+    })
+})
+
+app.get('/users/:id', function (req, res) {
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: 'got one user',
+        users: req.params.id
     })
 })
 
